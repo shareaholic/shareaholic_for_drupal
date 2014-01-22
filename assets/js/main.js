35938d5cd53cf7b4bb05909cfef35e4473753bba
Huy Ngu <hngu@shareaholic.com>
2014-01-22 10:30:25 -0500

    adding assets from wordpress

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,22 @@
 (function($) {
+  window.Shareaholic = window.Shareaholic || {};
 
-  $(function(){
-    // jquery code
+  $(document).ready(function() {
+
+    $('#terms_of_service_modal').reveal({
+      closeonbackgroundclick: false,
+      closeonescape: false,
+      topPosition: 50
+    });
+
+    $('#get_started').on('click', function(e) {
+      //e.preventDefault();
+      //data = {action: 'shareaholic_accept_terms_of_service'};
+      // $('#terms_of_service_modal').trigger('reveal:close');
+      //Shareaholic.submit_to_admin(data, function(){
+      //  location.reload();
+      //});
+    })
+
   });
-
 })(sQuery);
