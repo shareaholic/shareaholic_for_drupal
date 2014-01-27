4922c763197dbd8b48c41fe189e59e75ea3a4ee2
Huy Ngu <hngu@shareaholic.com>
2014-01-27 13:25:13 -0500

    Fixing issues: removing php short tags and explicitly calling print

--- Rakefile
+++ Rakefile
@@ -5,3 +5,8 @@ task :makerelease do
     puts 'This does nothing yet'
 end
 
+task :makequickcopy, :path do |task, args|
+  sh "cp -R ../shareaholic_for_drupal #{args[:path]}"
+  sh "rm -rf #{args[:path]}/shareaholic_for_drupal/.git"
+end
+
--- templates/failed_to_create_api_key_modal.tpl.php
+++ templates/failed_to_create_api_key_modal.tpl.php
@@ -1,18 +1,18 @@
 <div class='reveal-modal blocking-modal api-key-modal' id='failed_to_create_api_key'>
-  <h4><?= t('Setup Shareaholic') ?></h4>
+  <h4><?php print t('Setup Shareaholic') ?></h4>
   <div class="content pal">
   <div class="line pvl">
     <div class="unit size3of3">
       <p>
-        <?= t('It appears that we are having some trouble setting up Shareaholic for WordPress right now. This is usually temporary. Please revisit this section after a few minutes or click "retry" now.'); ?>
+        <?php print t('It appears that we are having some trouble setting up Shareaholic for WordPress right now. This is usually temporary. Please revisit this section after a few minutes or click "retry" now.'); ?>
       </p>
     </div>
   </div>
   <div class="pvl">
-    <?= $variables['failed_api_key_modal']['hidden'] ?>
-    <?= $variables['failed_api_key_modal']['submit'] ?>
+    <?php print $variables['failed_api_key_modal']['hidden'] ?>
+    <?php print $variables['failed_api_key_modal']['submit'] ?>
     <br /><br />
-    <a href='/admin' style="font-size:12px; font-weight:normal;"><?= t('or, try again later'); ?></a>
+    <a href='/admin' style="font-size:12px; font-weight:normal;"><?php print t('or, try again later'); ?></a>
   </div>
   </div>
 </div>
