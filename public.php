20b0708cb743e61e5646beaab97e940c747dd74f
Huy Ngu <hngu@shareaholic.com>
2014-01-28 18:17:42 -0500

    Using site_id instead of apikey

--- public.php
+++ public.php
@@ -43,8 +43,8 @@ class ShareaholicPublic {
           shr.onload = shr.onreadystatechange = function() {
             var rs = this.readyState;
             if (rs && rs != 'complete' && rs != 'loaded') return;
-            var apikey = '$api_key';
-            try { Shareaholic.init(apikey); } catch (e) {}
+            var site_id = '$api_key';
+            try { Shareaholic.init(site_id); } catch (e) {}
           };
           var s = document.getElementsByTagName('script')[0];
           s.parentNode.insertBefore(shr, s);
