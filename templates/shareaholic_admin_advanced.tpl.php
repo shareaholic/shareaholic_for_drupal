fde75e0a3b53731a45ce1243066d8b31797a65b2
Huy Ngu <hngu@shareaholic.com>
2014-02-06 16:23:30 -0500

    Adding blank page for app manager

--- templates/shareaholic_admin_advanced.tpl.php
+++ templates/shareaholic_admin_advanced.tpl.php
@@ -1,11 +1,5 @@
 <?php
-  $module_path = drupal_get_path('module', 'shareaholic');
-  drupal_add_css($module_path . '/assets/css/reveal.css', array('group' => CSS_DEFAULT));
-  drupal_add_css($module_path . '/assets/css/main.css', array('group' => CSS_DEFAULT));
-  drupal_add_js($module_path . '/assets/js/jquery_custom.js', array('group' => JS_DEFAULT));
-  drupal_add_js($module_path . '/assets/js/jquery_ui_custom.js', array('group' => JS_DEFAULT));
-  drupal_add_js($module_path . '/assets/js/jquery.reveal.modified.js', array('group' => JS_DEFAULT));
-  drupal_add_js($module_path . '/assets/js/main.js', array('group' => JS_DEFAULT));
+  ShareaholicAdmin::include_css_js_assets();
   print '<div id="shareaholic-form-container">';
   print(drupal_render(drupal_get_form('shareaholic_advanced_settings_form')));
   print(drupal_render(drupal_get_form('shareaholic_reset_plugin_form')));
---
+++ templates/shareaholic_admin_settings.tpl.php
@@ -0,0 +1,5 @@
+<?php
+ ShareaholicAdmin::include_css_js_assets();
+?>
+<div id="shareaholic-form-container">
+</div>
