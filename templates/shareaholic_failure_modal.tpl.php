d04ac0b7de7d81466dd80f6f8669f06027559dc6
Huy Ngu <hngu@shareaholic.com>
2014-02-11 02:25:51 -0500

    changing wording from wordpress to drupal

--- templates/shareaholic_failure_modal.tpl.php
+++ templates/shareaholic_failure_modal.tpl.php
@@ -4,7 +4,7 @@
   <div class="line pvl">
     <div class="unit size3of3">
       <p>
-        <?php print t('It appears that we are having some trouble setting up Shareaholic for WordPress right now. This is usually temporary. Please revisit this section after a few minutes or click "retry" now.'); ?>
+        <?php print t('It appears that we are having some trouble setting up Shareaholic for Drupal right now. This is usually temporary. Please revisit this section after a few minutes or click "retry" now.'); ?>
       </p>
     </div>
   </div>
