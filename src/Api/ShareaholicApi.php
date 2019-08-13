2d4775ced894e1e046f0876661c0421fd3710ddd
Łukasz Zaroda <luken@luken-tech.pl>
2019-08-13 22:03:37 +0200

    Implemented integration with Content Manager API.

--- src/Api/ShareaholicApi.php
+++ src/Api/ShareaholicApi.php
@@ -13,6 +13,9 @@ use Psr\Log\LoggerInterface;
 class ShareaholicApi {
   const SERVICE_URL = 'https://www.shareaholic.com';
   const API_URL = 'https://web.shareaholic.com';
+  const CM_API_URL = 'https://cm-web.shareaholic.com';
+  const CM_SINGLE_PAGE_REFRESH_URL = self::CM_API_URL . '/jobs/uber_single_page';
+  const CM_DOMAIN_REFRESH_URL = self::CM_API_URL . '/jobs/single_domain';
   const HEALTH_CHECK_URL = self::API_URL . '/haproxy_health_check';
   const KEY_GENERATING_URL = self::API_URL . '/publisher_tools/anonymous';
   const EVENTS_URL = self::API_URL . '/api/events';
@@ -74,6 +77,7 @@ class ShareaholicApi {
     }
 
     $json_response = json_decode($data, TRUE);
+    $this->domainRefresh();
     return $json_response['api_key'];
   }
 
@@ -119,6 +123,46 @@ class ShareaholicApi {
     }
 
     return $response->getStatusCode() === 200;
+  }
+
+  /**
+   * Notify Shareaholic Content Manager if content has to be refreshed.
+   *
+   * @param string $url
+   */
+  public function singlePageRefresh($url) {
+    try {
+      $response = $this->post(self::CM_SINGLE_PAGE_REFRESH_URL, [
+        'args' => [$url, ['force' => TRUE]],
+      ]);
+    } catch (\Exception $exception) {
+      $this->logger->critical("Clearing Shareaholic cache of a '$url' page failed. Connection failed.");
+      return;
+    }
+
+    $statusCode = $response->getStatusCode();
+    if ($statusCode !== 200) {
+      $this->logger->critical("Clearing Shareaholic cache of a '$url' page failed. Status code returned: $statusCode.");
+    }
+  }
+
+  /**
+   * Notify Shareaholic Content Manager if domain has to be refreshed.
+   */
+  public function domainRefresh() {
+    try {
+      $response = $this->post(self::CM_DOMAIN_REFRESH_URL, [
+        'args' => [\Drupal::request()->getSchemeAndHttpHost(), ['force' => TRUE]],
+      ]);
+    } catch (\Exception $exception) {
+      $this->logger->critical("Clearing Shareaholic domain cache in Content Manager failed. Connection failed.");
+      return;
+    }
+
+    $statusCode = $response->getStatusCode();
+    if ($statusCode !== 200) {
+      $this->logger->critical("Clearing Shareaholic domain cache in Content Manager failed. Status code returned: $statusCode.");
+    }
   }
 
   /**
